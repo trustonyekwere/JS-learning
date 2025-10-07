65bd56bb14b1d559b4501f4dc4c674e82897d9c7
Trust Onyekwere <trustonyekwere151@gmail.com>
2025-10-07 21:54:46 +0100

    started learning JS on 07/10/2025

---
+++ index.js
@@ -0,0 +1,11 @@
+let age = 17; //number
+let firstname = "Tee"; //string
+let favfood = "garri"; //string
+
+console.log(typeof firstname);
+console.log(`my name is ${firstname}`);
+console.log(`i am ${age} years old`);
+console.log(`i like ${favfood}`);
+
+
+
---
+++ notes.md
@@ -0,0 +1,6 @@
+## Variables
+strings are used for letters. they can be written in '' or "" quotes.
+
+numbers can be written without quotes.
+
+booleans are either true or false.
